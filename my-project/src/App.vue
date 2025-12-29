7815d5b8211c56dc436a574f32d17d71325169f7
강희주 <k.lbtkr@gmail.com>
2025-12-29 17:19:20 +0900

    24. composition api 사용법 1/2

--- my-project/src/App.vue
+++ my-project/src/App.vue
@@ -1,9 +1,33 @@
+<!--setup이 있으면 옵션 구분 없이 한 번에 선언할 수 있음-->
 <script setup>
-    
+    import { ref, onMounted, watch } from 'vue';
+    import ChildComp from './components/ChildComp.vue';
+
+    let count = ref(0); // 상태변수 초기값(state)
+    let title = "Hello Vue";
+    let color = ref('blue');
+
+    onMounted(() => {
+        console.log('mounted');
+    });
+
+    const increaseCount = () => {
+        count++;
+    }
+
+    watch(color, () => {
+        console.log('color 변경');
+    });
 </script>
 
 <template>
-
+    <div>
+        <h1>{{ title }}</h1>
+        <button @click="increaseCount">count++</button>
+        <p>{{ count }}</p>
+        <ChildComp :color="color" bgColor="yellow"/>
+        <button @click="color='red'">change color</button>
+    </div>
 </template>
 
 <style scoped>
---
+++ my-project/src/components/ChildComp.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <h2>Child Component</h2>
+        <p :style="{color,
+                    backgroundColor: bgColor}
+        ">
+            {{ color }}
+        </p>
+    </div>
+</template>
+<script setup>
+    const props = defineProps({
+        color: String,
+        bgColor: String,
+    });
+    console.log(props.color);
+</script>
+<style>
+    
+</style>
