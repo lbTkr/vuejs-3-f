35fce8f6435b48d17432925b9a26e2d119336269
강희주 <k.lbtkr@gmail.com>
2026-01-02 13:57:07 +0900

    router 수정

--- portfolio/src/App.vue
+++ portfolio/src/App.vue
@@ -1,36 +1,10 @@
 <script setup>
-    import TitleComp from './components/Title.vue';
-    import PortfolioComp from './components/Portfolio.vue';
-    import AboutComp from './components/About.vue';
+    import Navbar from './components/Navbar.vue';
 </script>
 
 <template>
-<nav class="navbar navbar-expand-lg fixed-top bg-body-tertiary" data-bs-theme="dark">
-    <div class="container-fluid">
-        <a class="navbar-brand" href="#">Navbar</a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav">
-                <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="#title">Home</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#portfolio">Portfolio</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#about">About</a>
-                </li>
-            </ul>
-        </div>
-    </div>
-</nav>
-
-<TitleComp />
-<PortfolioComp />
-<AboutComp />
-
+<Navbar />
+<router-view />
 </template>
 
 <style scoped lang="scss">
--- portfolio/src/router/router.js
+++ portfolio/src/router/router.js
@@ -1,12 +1,16 @@
 import {createWebHistory, createRouter} from 'vue-router';
 import TitleComp from '../components/Title.vue';
+import About from '../components/About.vue';
+import Portfolio from '../components/Portfolio.vue';
+import Detail from '../components/Detail.vue';
 
 const router = createRouter({
     history: createWebHistory(),
     routes: [
         {path: '/', component: TitleComp},
-        // {path: '/portfolio', component: Portfolio},
-        // {path: '/about', component: About}
+        {path: '/portfolio', component: Portfolio},
+        {path: '/about', component: About},
+        {path: '/detail/:id', component: Detail}
     ],
 });
 
